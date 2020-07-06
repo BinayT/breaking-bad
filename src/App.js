13fe730fb4ae84e475415d21067701197aadfa01
Binay <binayprogramming@gmail.com>
2020-07-06 18:56:01 +0200

    Added CharacterGrid which is gonna show us the characters

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import "./App.css";
 import Header from "./components/ui/Header";
+import CharacterGrid from "./components/characters/CharacterGrid";
 
 const App = () => {
   const [items, setItems] = useState([]);
@@ -12,7 +13,8 @@ const App = () => {
       const result = await axios(
         `https://www.breakingbadapi.com/api/characters`
       );
-      console.log(result.data);
+      setItems(result.data);
+      setIsLoading(false);
     };
     fetchItems();
   }, []);
@@ -20,6 +22,7 @@ const App = () => {
   return (
     <div className="container">
       <Header />
+      <CharacterGrid isLoading={isLoading} items={items} />
     </div>
   );
 };
